91cc4ddb49dfc988267dc773d8624cd26212176c
Adam Taylor <adamt@couchbase.com>
2014-07-22 15:27:03 +0100

    Removed graphing due to performance degrade

--- response_logger/response_logger.py
+++ response_logger/response_logger.py
@@ -4,13 +4,6 @@ from couchbase import Couchbase
 from pprint import pprint
 import time
 import datetime
-import numpy
-import matplotlib.pyplot as plt
-
-# Create graph object
-fig=plt.figure()
-plt.ion()
-plt.show()
 
 # Initialise time keeping variable
 i = 0
@@ -22,19 +15,15 @@ with open('Responses', 'a') as f:
   while(True):
 
     before = datetime.datetime.now()
-    rows = cb.query("beer", "by_name", stale=False) 
+    rows = cb.query("beer", "by_name", stale=False)
     after = datetime.datetime.now()
 
     offset = after - before # Calculate the response time
     offset = int(offset.microseconds)
 
-    if (offset > 500): # Log any high response times
+    if (offset > 300): # Log any high response times
       f.write("Response time of "+str(offset)+"at point"+str(i))
 
-    print runningTime
-    plt.scatter(i,offset)
-    plt.draw()
-    plt.show()
     print offset
     i+=1
     time.sleep(0.2) # 5 requests per sec
